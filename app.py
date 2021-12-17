9c3ac42779229a235848d384defce1933aa25738
DeepScienceData <khereddinegeorges@gmail.com>
2021-12-17 13:38:14 +0100

    Add New Profile

--- app.py
+++ app.py
@@ -68,6 +68,17 @@ def credit(id_client):
 
     return jsonify(output)
 
+@app.route('/predict_api',methods=['POST'])
+def predict_api():
+    '''
+    For direct API calls trought request
+    '''
+    data = request.get_json(force=True)
+    prediction = clf.predict([np.array(list(data.values()))])
+
+    output = prediction[0]
+    return jsonify(output)
+
 #lancement de l'application
 if __name__ == "__main__":
         app.run(debug=True)
