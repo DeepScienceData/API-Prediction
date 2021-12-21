de03c0a2663eb23dc917f82350a84c75098ab417
DeepScienceData <khereddinegeorges@gmail.com>
2021-12-21 08:37:01 +0100

    Add new procfile

--- app.py
+++ app.py
@@ -7,36 +7,6 @@ from zipfile import ZipFile
 app = Flask(__name__)
 model = pickle.load(open('./model/RandomForestClassifier.pkl', 'rb'))
 
-#----------------------------------- Functions ---------------------------------------#
-def load_data():
-        z = ZipFile("data/data_default_risk.zip")
-        data = pd.read_csv(z.open('application_train.csv'), index_col='SK_ID_CURR', encoding ='utf-8')
-
-        z = ZipFile("data/X_sample_30.zip")
-        sample = pd.read_csv(z.open('X_sample.csv'), index_col='SK_ID_CURR', encoding ='utf-8')
-
-        return data, sample
-
-def load_model():
-        '''loading the trained model'''
-        pickle_in = open('model/RandomForestClassifier.pkl', 'rb') 
-        clf = pickle.load(pickle_in)
-        return clf
-
-def identite_client(data, id):
-        data_client = data[data.index == int(id)]
-        return data_client
-
-def load_prediction(sample, id, clf):
-        X=sample.iloc[:, :126]
-        score = clf.predict_proba(X[X.index == int(id)])[:,1]
-        predict = clf.predict(X[X.index == int(id)])
-        return score, predict
-
-#Chargement des donner :
-data, sample = load_data()
-id_client = sample.index.values
-clf = load_model()
 
 @app.route("/")
 def hello():
