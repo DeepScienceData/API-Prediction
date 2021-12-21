73938d0b5d2890506d8a262a5b5509ba9c9f6c9f
DeepScienceData <khereddinegeorges@gmail.com>
2021-12-21 08:01:29 +0100

    Add new procfile

---
+++ app.py
@@ -0,0 +1,33 @@
+import numpy as np
+from flask import Flask, request, jsonify, render_template
+import pickle
+import pandas as pd
+
+app = Flask(__name__)
+model = pickle.load(open('./model/RandomForestClassifier.pkl', 'rb'))
+
+
+@app.route("/")
+def hello():
+    """
+    Ping the API.
+    """
+    return jsonify({"text":"Hello, the API is up and running..." })
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    # parse input features from request
+    request_json = request.get_json()
+    df = pd.json_normalize(request_json)
+ 
+    # load model
+    prediction = model.predict_proba(df)[:, 1][0]
+    # Format prediction in percentage with 2 decimal points
+    prediction = "The client has a " + str(round(prediction*100,2)) + "% risk of defaulting on their loan."
+    print("prediction: ", prediction)
+
+    # Return output
+    return jsonify(json.dumps(str(prediction)))
+
+if __name__ == '__main__':
+    app.run(debug=True)
