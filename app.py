c396173cd5d30fe9cef9167f4683e92cc4c9936a
DeepScienceData <khereddinegeorges@gmail.com>
2021-12-21 08:37:55 +0100

    Add new procfile

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from flask import Flask, request, jsonify, render_template
 import pickle
 import pandas as pd
 from zipfile import ZipFile
+import json
 
 app = Flask(__name__)
 model = pickle.load(open('./model/RandomForestClassifier.pkl', 'rb'))
